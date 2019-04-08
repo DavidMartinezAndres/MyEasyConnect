a4ddded1138ac0a406d6b4dd83da504d739a9b91
David Martinez <d.martinez@innovation.es>
2019-04-08 15:28:32 +0200

    Mail model added

---
+++ MyEasyConnect/Models/Mail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEasyConnect.Models
+{
+    public class Mail
+    {
+        public int Id { get; set; }
+        public string status { get; set; }
+        public string Subject { get; set; }
+        public string MailDate { get; set; }
+        public string Title { get; set; }
+        public string MessageBody { get; set; }
+    }
+}
