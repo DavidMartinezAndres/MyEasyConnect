eeb2704362a71854acbc3cf38b7333170592484a
David Martinez <d.martinez@innovation.es>
2019-04-08 16:38:27 +0200

    Mail updates, getWorker started

--- MyEasyConnect/Controllers/MyEasyConnectController.cs
+++ MyEasyConnect/Controllers/MyEasyConnectController.cs
@@ -1,24 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MyEasyConnect.Models;
+using Oracle.ManagedDataAccess.Client;
 using System.Configuration;
-using System.Linq;
-using System.Web;
+using System.Data;
+using System.Text;
+
 using System.Web.Http;
 
 namespace MyEasyConnect.Controllers
 {
     public class MyEasyConnectController : ApiController
     {
+
         readonly string connectionString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=" + ConfigurationManager.AppSettings["DBHost"] + ")" +
             "(PORT=" + ConfigurationManager.AppSettings["DBPort"] + "))" +
             "(CONNECT_DATA=(SERVER=dedicated)(SERVICE_NAME=" + ConfigurationManager.AppSettings["DBServiceName"] + ")));" +
             "User Id=" + ConfigurationManager.AppSettings["DBUser"] + ";Password=" + ConfigurationManager.AppSettings["DBPassword"] + ";";
 
-        [Route("helloworld")]
-        public String Get()
+        [Route("getWorker/{id}")]
+        public Worker GetWorker(int id)
         {
-            String x = "Hello World!";
-            return x;
+            StringBuilder sql = new StringBuilder();
+            sql.Append("SELECT IDWORKER            AS \"IdWorker\", ");
+            sql.Append("       W.NAME              AS \"Name\", ");
+            sql.Append("       FIRST_SURNAME       AS \"FirstSurname\", ");
+            sql.Append("       SECOND_SURNAME      AS \"SecondSurname\", ");
+            sql.Append("       PROFILE_PICTURE     AS \"ProfilePicture\", ");
+            sql.Append("       POINTS, ");
+            sql.Append("       J.NAME              AS \"Job\" ");
+            sql.Append("  FROM DMARTINEZ.WORKER W INNER JOIN JOB J ON W.IDJOB = J.ID ");
+            sql.Append(" WHERE W.IDWORKER = :IDWORKER ");
+
+            using (OracleConnection conn = new OracleConnection(connectionString))
+            {
+                conn.Open();
+
+                using (OracleCommand cmd = new OracleCommand())
+                {
+                    cmd.Connection = conn;
+
+                    cmd.CommandText = sql.ToString();
+
+                    cmd.BindByName = true;
+                    cmd.CommandType = CommandType.Text;
+
+                    cmd.Parameters.Add("IDWORKER", OracleDbType.Int32).Value = id;
+
+                    DataTable table = new DataTable();
+
+                    Worker response = new Worker();
+                    return response;
+                    /*using (OracleDataAdapter data = new OracleDataAdapter(cmd))
+                    {
+                        data.Fill(table);
+
+                        return response;
+                    }*/
+                }
+            }
         }
 
     }
