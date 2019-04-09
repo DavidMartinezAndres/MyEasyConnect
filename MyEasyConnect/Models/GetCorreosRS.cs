703e49eb33884d6184fc0979e029d16a091e3050
David Martinez <d.martinez@innovation.es>
2019-04-09 11:27:13 +0200

    Model for getCorreos done

---
+++ MyEasyConnect/Models/GetCorreosRS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEasyConnect.Models
+{
+    public class GetCorreosRS
+    {
+        public List<Mail> Mails { get; set; }
+    }
+}
